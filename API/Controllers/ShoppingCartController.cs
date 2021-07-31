0f3576935ec9d04ab15ffb8e873a0021b127a808
Michael Walker <michaelwalker846@yahoo.com>
2021-07-31 18:30:08 -0400

    Section 16 - API Validation

--- API/Controllers/ShoppingCartController.cs
+++ API/Controllers/ShoppingCartController.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -8,8 +10,10 @@ namespace API.Controllers
     public class ShoppingCartController : BaseApiController
     {
         private readonly IShoppingCartRepository _shoppingCartRepository;
-        public ShoppingCartController(IShoppingCartRepository shoppingCartRepository)
+        private readonly IMapper _mapper;
+        public ShoppingCartController(IShoppingCartRepository shoppingCartRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _shoppingCartRepository = shoppingCartRepository;
         }
 
@@ -22,9 +26,11 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<CustomerShoppingCart>> UpdateShoppingCart(CustomerShoppingCart shoppingCart)
+        public async Task<ActionResult<CustomerShoppingCart>> UpdateShoppingCart(CustomerShoppingCartDto shoppingCart)
         {
-            var updatedShoppingCart = await _shoppingCartRepository.UpdateShoppingCartAsync(shoppingCart);
+            var customerShoppingCart = _mapper.Map<CustomerShoppingCartDto, CustomerShoppingCart>(shoppingCart);
+
+            var updatedShoppingCart = await _shoppingCartRepository.UpdateShoppingCartAsync(customerShoppingCart);
 
             return Ok(updatedShoppingCart);
         }
